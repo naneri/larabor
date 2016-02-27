1fca0e03bf7640dc8cde273d4f690846d00a47f2
Kanat Tabladiev <ktnaneri@gmail.com>
2016-02-28 01:52:17 +0600

    metrics added

--- resources/views/_misc/_layout.blade.php
+++ resources/views/_misc/_layout.blade.php
@@ -162,6 +162,34 @@
   ga('send', 'pageview');
 
 </script>
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript">
+  (function (d, w, c) {
+    (w[c] = w[c] || []).push(function() {
+      try {
+        w.yaCounter35715450 = new Ya.Metrika({
+          id:35715450,
+          clickmap:true,
+          trackLinks:true,
+          accurateTrackBounce:true
+        });
+      } catch(e) { }
+    });
+
+    var n = d.getElementsByTagName("script")[0],
+      s = d.createElement("script"),
+      f = function () { n.parentNode.insertBefore(s, n); };
+    s.type = "text/javascript";
+    s.async = true;
+    s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+    if (w.opera == "[object Opera]") {
+      d.addEventListener("DOMContentLoaded", f, false);
+    } else { f(); }
+  })(document, window, "yandex_metrika_callbacks");
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/35715450" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
 @include('_misc._footer')
 @include('_partials._alerts')
 </body>
