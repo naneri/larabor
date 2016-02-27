fdc2bbe167b9828800272737788c62142172fe3f
Kanat Tabladiev <ktnaneri@gmail.com>
2016-02-27 23:11:30 +0600

    fix for the google analytics

--- resources/views/_misc/_layout.blade.php
+++ resources/views/_misc/_layout.blade.php
@@ -156,7 +156,7 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
   @if(Auth::check())
-  ga('set', 'userId', {{Auth::id()}}); // Set the user ID using signed-in user_id.
+  ga('set', 'userId', '{{Auth::id()}}'); // Set the user ID using signed-in user_id.
   @endif
   ga('create', 'UA-73364271-1', 'auto');
   ga('send', 'pageview');
