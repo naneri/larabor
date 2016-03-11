4c7e894ca38dd38435f0ca623dd6a5c968884aba
Kanat Tabladiev <ktnaneri@gmail.com>
2016-03-11 23:56:00 +0600

    post deploy script updated

--- update.sh
+++ update.sh
@@ -6,3 +6,7 @@ php artisan migrate
 echo "migrated"
 bower install --allow-root
 echo "bower dependencies updated"
+php artisan route:cache
+php artisan config:cache
+php artisan optimize
+echo "routes and config cached"
