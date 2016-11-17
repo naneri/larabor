4a71c6649d1d7e48df5133dec331147cfd6501e6
Kanat Tabaldiev <ktnaneri@gmail.com>
2016-11-18 03:02:48 +0600

    fixed the bug

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -46,5 +46,6 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Foundation\Http\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'is_admin'  => \App\Http\Middleware\AdminMiddleware::class,
     ];
 }
