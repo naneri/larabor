eb1c546d3ecb71823a200daac9be8e09e82c1389
Kanat Tabaldiev <ktnaneri@gmail.com>
2016-11-25 02:40:54 +0600

    another update

--- config/zabor.php
+++ config/zabor.php
@@ -3,7 +3,7 @@
 return [
     
     // the number by which the ads are increased on the main page
-    'ad_increase_number' => 1813,
+    'ad_increase_number' => 1300,
 
     // IDs of the users affiliated with the website
     'affiliates'    => [2, 25],
